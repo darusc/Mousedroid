c1affa6662907091986c5ff2e29aea1953c1a9b0
hypertensiune <dariuscirneala05@gmail.com>
2025-07-18 19:04:31 +0300

    Added adb + possible missing binaries

--- server/install.bat
+++ server/install.bat
@@ -2,6 +2,15 @@
 
 copy %WXWIN%\lib\gcc_dll\wxbase32u_gcc_custom.dll cmake\bin
 copy %WXWIN%\lib\gcc_dll\wxmsw32u_core_gcc_custom.dll cmake\bin
+
+copy adb\adb.exe cmake\bin
+copy adb\AdbWinApi.dll cmake\bin
+copy adb\AdbWinUsbApi.dll cmake\bin
+
+copy dll\libgcc_s_seh-1.dll cmake\bin
+copy dll\libstdc++-6.dll cmake\bin
+copy dll\libwinpthread-1.dll cmake\bin
+
 copy /y app.ico cmake\bin
 
 (
@@ -10,5 +19,3 @@ copy /y app.ico cmake\bin
     echo RUN_STARTUP=0
     echo SCROLL_SENSITIVITY=3 
 )>cmake\bin\config.ini
-
-
